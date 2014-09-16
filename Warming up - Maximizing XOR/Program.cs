90f8882c42735419a5f1d1dd4481b5a8c5990668
Sjoerde <sjoerdvanegmond@gmail.com>
2014-09-16 21:51:10 +0200

    Add Maximizing XOR
    Add Maximizing XOR

---
+++ Warming up - Maximizing XOR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    /*
+     * Complete the function below.
+     */
+    static int maxXor(int l, int r)
+    {
+        return Enumerable.Range(l, r - l + 1).SelectMany(i => Enumerable.Range(l, r - l + 1).Select<int, int>(j => i ^ j)).Max();
+    }
+
+    static void Main(String[] args)
+    {
+        int res;
+        int _l;
+        _l = Convert.ToInt32(Console.ReadLine());
+
+        int _r;
+        _r = Convert.ToInt32(Console.ReadLine());
+
+        res = maxXor(_l, _r);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
