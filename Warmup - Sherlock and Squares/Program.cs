210a53e77ba4d9dd88f4dc86eeacc6180f5fd1f5
Sjoerde <sjoerdvanegmond@gmail.com>
2014-09-17 18:19:52 +0200

    Add Warmup - Sherlock and Squares
    Add Warmup - Sherlock and Squares

---
+++ Warmup - Sherlock and Squares/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    public static int NumberOfSquares(int left, int right)
+    {
+        int squareLeft = (int)Math.Ceiling(Math.Sqrt(left));
+        int squareRight = (int)Math.Floor(Math.Sqrt(right));
+        return squareRight - squareLeft + 1;
+    }
+
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int T = int.Parse(Console.ReadLine());
+
+        var tests = new List<Tuple<int, int>>();
+        for (int i = 0; i < T; i++)
+        {
+            var line = Console.ReadLine().Split(' ');
+            tests.Add(new Tuple<int, int>(int.Parse(line[0].Trim()), int.Parse(line[1].Trim())));
+        }
+
+        foreach (var test in tests)
+        {
+            Console.WriteLine(NumberOfSquares(test.Item1, test.Item2));
+        }
+    }
+}
