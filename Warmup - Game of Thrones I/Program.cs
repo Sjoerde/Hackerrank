5b188bc710e88fed284be43946ccf8c9ebd38d6d
Sjoerd van Egmond <Sjoerd.vanEgmond@ortec-finance.com>
2014-09-17 14:10:29 +0200

    Add Warmup - GoT I
    Add Warmup - GoT I

---
+++ Warmup - Game of Thrones I/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+internal class Solution
+{
+	private static void Main(String[] args)
+	{
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+		string line = Console.ReadLine();
+
+		var chars = line.ToCharArray().GroupBy(c => c).Select(g => new {g.Key, Count = g.Count()});
+		if (chars.Count(c => c.Count%2 == 1) > 1)
+		{
+			Console.WriteLine("NO");
+		}
+		else
+		{
+			Console.WriteLine("YES");
+		}
+	}
+}
